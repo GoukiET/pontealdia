124619d5652ac69aa620796e9c8acacd98bc965c
Sebastián Cubillos <97460797+GoukiET@users.noreply.github.com>
2022-07-22 13:01:20 -0400

    Add files via upload
    nuevo repo

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+let productos = [];
+
+let producto = {
+    
+}
+
+function agregarProducto(){ //CREATE
+
+}
+
+function obtenerProductos(){ //READ
+
+}
+
+function editarProducto(){ //UPDATE
+
+}
+
+function eliminarProducto(){ //DELETE
+
+}
+
+function filtrarProducto(){
+
+}
+
+function ordenarProductos(){
+
+}
